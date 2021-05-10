68f7b488d2f669f8359d17b294a1eacc862519db
Don Choi <donchoi25@gmail.com>
2021-05-09 20:31:04 -0500

    player sprite added

--- main.py
+++ main.py
@@ -3,20 +3,38 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((480,480))
+screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption("Moving Around")
 pygame.mouse.set_visible(0)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, image, height, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.speed = speed
+        self.image = image
+        self.pos = image.get_rect().move(0,height)
+    def move(self):
+        self.pos = self.pos.move(0, self.speed)
+        if self.pos.right > 600:
+            self.pos.left = 0
 
-#game loop, game never runs faster than 60 frames
-clock = pygame.time.Clock()
+#creating player 1
+playerimg = pygame.image.load('player.png').convert()
+playerimg = pygame.transform.scale(playerimg, (40,40))
+player1 = Player(playerimg,0,0)
+
+#creating background
+#background = pygame.image.load('bg.jpeg').convert()
+#screen.blit(background, (0, 0))
+
+#main game loop
 while True:
-    clock.tick(60)
+    screen.blit(player1.image, player1.pos)
     
     #Event listener
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit() 
+            
+    pygame.display.update()
+    pygame.time.delay(100)
