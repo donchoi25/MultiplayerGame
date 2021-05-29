20c82e94925b15ced12aaae22af94c314119c296
Don Choi <donchoi25@gmail.com>
2021-05-29 12:53:53 -0500

    multiple clients threaded

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import socket
 import json     
 import atexit    
 import re    
+import _thread
+from threading import RLock
 
 s = socket.socket()         # Create a socket object
 host = '127.0.0.1'    #ip address of machine
@@ -13,6 +15,36 @@ playerid = 0
 #creating map that tracks player information
 playerLocation = {}
 
+#create a lock
+lock = RLock()
+
+#create function for new client threads
+def on_new_client(clientsocket,addr):
+   global playerid
+   while True:
+      msg = clientsocket.recv(1024).decode()
+      #Seperates all messages into a list. Assume all messages are enclosed in {}
+      receivedList = [e + "}" for e in msg.split("}") if e]
+
+      for received in receivedList:
+         #if a new player is being created, assign a unique player id
+         if received == "{PlayerCreated}":
+            with lock:
+               clientsocket.sendall(str(playerid).encode())
+               playerid += 1
+            print("Player Created ID: ", playerid)
+         #location information on server needs to be updated
+         elif received != None:
+            #turn json into dic
+            jsonDic = json.loads(received)
+            #update the dictionary
+            with lock:
+               playerLocation[jsonDic["id"]] = (jsonDic["posx"], jsonDic["posy"])
+               print(playerLocation)
+
+   clientsocket.close()
+
+
 #closes the port on program exit
 def exit_handler():
     print("port successfully closed")
@@ -22,25 +54,9 @@ atexit.register(exit_handler)
 
 #5 players possible
 s.listen(5)  
-#accepts any new connections
-c, addr = s.accept()                    
+                   
 
 while True:
-   orig = c.recv(1024).decode()
-   #Seperates all messages into a list. Assume all messages are enclosed in {}
-   receivedList = [e + "}" for e in orig.split("}") if e]
-   #Process all messages
-   for received in receivedList:
-      #if a new player is being created, assign a unique player id
-      if received == "{PlayerCreated}":
-         c.sendall(str(playerid).encode())
-         print("Player Created ID: ", playerid)
-         playerid += 1
-      #location information on server needs to be updated
-      elif received != None:
-         #turn json into dic
-         print(received)
-         jsonDic = json.loads(received)
-         #update the dictionary
-         playerLocation[jsonDic["id"]] = (jsonDic["posx"], jsonDic["posy"])
-         print(playerLocation)
+   #accepts any new connections
+   c, addr = s.accept() 
+   _thread.start_new_thread(on_new_client,(c,addr))
